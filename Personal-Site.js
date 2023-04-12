9fc4321ae7e87ca1a2a138641606b5bd0981669a
ok-coder1 <106904247+ok-coder1@users.noreply.github.com>
2023-04-12 17:26:19 +0530

    Update Personal-Site.js

--- Personal-Site.js
+++ Personal-Site.js
@@ -4,5 +4,5 @@ const execSync = require('child_process').execSync;
 const output = execSync('ps aux | grep -v grep | grep -c open-url.py', { encoding: 'utf-8' });
 if (output == 1)
 {
-  window.location.replace("ok-coder1.github.io/Personal-Site/actual-site.html");
+  window.location.replace("https://ok-coder1.github.io/Personal-Site/actual-site.html");
 }
