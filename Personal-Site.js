a167c1407903632744373cd180ac8e85e9e4c9ec
ok-coder1 <106904247+ok-coder1@users.noreply.github.com>
2023-04-12 17:22:05 +0530

    Update Personal-Site.js

--- Personal-Site.js
+++ Personal-Site.js
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 
 const execSync = require('child_process').execSync;
-const output = execSync('ps aux | grep -v grep | grep -c python3 /Users/User/Desktop/tkdesigner-figma/login-signup-form/login-signup/build/open-url.py', { encoding: 'utf-8' });
+const output = execSync('ps aux | grep -v grep | grep -c open-url.py', { encoding: 'utf-8' });
 if (output == 1)
 {
   window.location.replace("ok-coder1.github.io/Personal-Site/actual-site
